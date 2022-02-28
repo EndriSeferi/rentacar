d7b18f8622fb5e92ade6be9b77d87f4585e6b1ba
Endri Seferi <endriseferi98@gmail.com>
2022-02-28 17:07:03 +0100

    Version 1.0.1

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use("/api/bookings/", require("./routes/bookingsRoute"));
 
 const path = require("path");
 
-if (process.env.PORT === "production" || 5000) {
+if (process.env.NODE_ENV === "production") {
   app.use("/", express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client/build/index.html"));
